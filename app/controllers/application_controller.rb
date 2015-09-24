95cde1674f461de867358f09ddba51840fb80418
selasiehanson <soundfever18@gmail.com>
2015-09-24 02:49:20 +0000

    removed uncessary code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def error(model, klass = '')
-    res = {
-      code: '',
-      error: ''            
-    }    
-
+    res = {}
     unless model.valid? 
       res[:error] = 'Some validation errors have been encountered'
       res[:code] = 'validation_errors'
